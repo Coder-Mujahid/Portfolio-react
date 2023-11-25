5026957f66f8754593a4bef50f2351eabddc508e
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-11-26 00:06:38 +0600

    add type animation and edit some color

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,4 +1,5 @@
 import img from "../../public/img/mujahid.png";
+import bd from '../../public/img/bd logo.png'
 export default function Sidebar() {
   return (
     <div className=" col-span-1 flex flex-col justify-center w-full h-screen">
@@ -6,16 +7,16 @@ export default function Sidebar() {
           <div className="md:flex  items-center px-10">
         <div>
           <aside className="w-44 rounded-lg ">
-            <img className="w-full rounded-md bg-gradient-to-tl from-lime-100 to-green-200" src={img} alt="profile" />
+            <img className="w-full rounded-md bg-gradient-to-r from-[#83c5be] to-[#457b9d]" src={img} alt="profile" />
           </aside>
         </div>
-        <div className=" capitalize flex flex-col pl-5 justify-end h-full gap-1 w-full">
+        <div className=" capitalize flex flex-col pl-5 justify-end h-full gap-2 w-full">
           {/* <h2 className=" text-3xl font-semibold text-slate-800 text-center w-full">Welcome to my portfolio  </h2> */}
           <h2 className=" text-2xl font-semibold text-blue-500">
-            md. mujahid islam{" "}
+            md. mujahidul islam
           </h2>
-          <h2 className=" text-lg font-semibold">frontend web developer</h2>
-          <small className=" font-semibold">from bangladesh</small>
+          <ExampleComponent></ExampleComponent>
+          <small className=" font-semibold"><img className=" w-[30px] inline-block mr-2 h-[16px]" src={bd} alt="bd logo" />from bangladesh</small>
           <ul className="flex text-3xl  ">
             <li>
               <i className="bx bxl-linkedin-square pr-4 text-[#0077b5]"></i>
@@ -39,34 +40,67 @@ export default function Sidebar() {
       <div className=" capitalize p-10">
         <ul className=" flex flex-col gap-4 mb-5">
           <li className=" flex items-center ">
-            <i className="bx bxs-phone text-2xl rounded-lg text-blue-500  py-2 px-3 bg-gradient-to-tl from-lime-100 to-green-200"></i>
+            <i className="bx bxs-phone shadow-xl shadow-slate-400 text-2xl rounded-lg  py-2 px-3 bg-gradient-to-r from-[#83c5be] to-[#457b9d]"></i>
             <div className=" flex flex-col font-bold pl-6">
               <small>Phone</small>
-              <small>+8801737705511</small>
+              <small className=" text-blue-600 tracking-wide">+8801737705511</small>
             </div>
           </li>
 
           <li className=" flex items-center">
-            <i className="bx h-full bxs-envelope text-2xl rounded-lg text-red-500  py-2 px-3 bg-gradient-to-tl from-lime-100 to-green-200"></i>
+            <i className="bx h-full bxs-envelope shadow-xl shadow-slate-400 text-2xl rounded-lg  py-2 px-3 bg-gradient-to-r from-[#83c5be] to-[#457b9d]"></i>
             <div className=" flex flex-col justify-between font-bold pl-6">
               <small>Email</small>
-              <small>mujahidislam400301@gmail.com</small>
+              <small className=" text-blue-600 tracking-wide">mujahidislam400301@gmail.com</small>
             </div>
           </li>
 
           <li className=" flex items-center">
-            <i className="bx bxs-location-plus text-2xl rounded-lg text-black  py-2 px-3 bg-gradient-to-tl from-lime-100 to-green-200"></i>
+            <i className="bx bxs-location-plus shadow-xl shadow-slate-400 text-2xl rounded-lg  py-2 px-3 bg-gradient-to-r from-[#83c5be] to-[#457b9d]"></i>
             <div className=" flex flex-col justify-between font-bold pl-6">
               <small>location</small>
-              <small>Thakurgaon , Rongpur, Bangladesh</small>
+              <small className=" text-blue-600 tracking-wide">Thakurgaon , Rongpur, Bangladesh</small>
             </div>
           </li>
         </ul>
 
-        <button className="font-semibold capitalize text-white text-sm py-2 w-full bg-slate-500 rounded-lg hover:text-black hover:bg-lime-300 transition duration-700 ease-in">
+        <button className="font-semibold capitalize text-black shadow-xl shadow-slate-400 text-sm py-2 w-full bg-[#4cc9f0] rounded-lg hover:text-white hover:tracking-wider hover:bg-[#00b4d8] transition duration-500 ease-in">
           View my resume
         </button>
       </div>
     </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+// text animation 
+import { TypeAnimation } from 'react-type-animation';
+ 
+const ExampleComponent = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        'frontend web developer', // Types 'One'
+        1000, // Waits 1s
+        'Junior Frontend Developer', // Deletes 'One' and types 'Two'
+        1000, // Waits 2s
+        'Frontend Architect', // Types 'Three' without deleting 'Two'
+        () => {
+          console.log('Sequence completed');
+        },
+      ]}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '18px',fontWeight:'600', display: 'inline-block' }}
+    />
   )
 }
