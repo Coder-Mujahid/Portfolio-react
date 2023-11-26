80f0f0a95c62f13946cb47118651ae7e6b5eecb0
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-11-26 23:46:53 +0600

    responsive the menu bar and change some icon

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,13 +1,19 @@
 import img from "../../public/img/mujahid.png";
-import bd from '../../public/img/bd logo.png'
+import bd from "../../public/img/bd logo.png";
+import phone from "../../public/img/icons8-phone.gif";
+import email from "../../public/img/icons8-email.gif";
+import location from "../../public/img/icons8-location.gif";
 export default function Sidebar() {
   return (
     <div className=" col-span-1 flex flex-col justify-center w-full min-h-screen">
-
-          <div className="md:flex items-center mb-10 md:mb-0 pt-5 md:px-10">
+      <div className="md:flex items-center mb-10 md:mb-0 pt-5 md:px-10">
         <div className=" flex items-center lg:justify-center w-full">
           <aside className="w-full md:w-44  rounded-lg mb-5 md:mb-0">
-            <img className="w-full rounded-md bg-gradient-to-r from-[#83c5be] to-[#457b9d]" src={img} alt="profile" />
+            <img
+              className="w-full rounded-md bg-gradient-to-r from-[#83c5be] to-[#457b9d]"
+              src={img}
+              alt="profile"
+            />
           </aside>
         </div>
         <div className=" capitalize flex flex-col md:pl-5 md:justify-end md:h-full gap-2  w-full">
@@ -16,7 +22,14 @@ export default function Sidebar() {
             md. mujahidul islam
           </h2>
           <ExampleComponent></ExampleComponent>
-          <small className=" font-semibold"><img className=" w-[30px] inline-block mr-2 h-[16px]" src={bd} alt="bd logo" />from bangladesh</small>
+          <small className=" font-semibold">
+            <img
+              className=" w-[30px] inline-block mr-2 h-[16px]"
+              src={bd}
+              alt="bd logo"
+            />
+            from bangladesh
+          </small>
           <ul className="flex text-3xl  ">
             <li>
               <i className="bx bxl-linkedin-square pr-4 text-[#0077b5]"></i>
@@ -40,26 +53,44 @@ export default function Sidebar() {
       <div className=" capitalize md:p-10 mb-5">
         <ul className=" flex flex-col gap-4 mb-5">
           <li className=" flex items-center ">
-            <i className="bx bxs-phone shadow-xl shadow-slate-400 text-2xl rounded-lg  py-2 px-3 bg-gradient-to-r from-[#83c5be] to-[#457b9d]"></i>
+            <img
+              className=" w-12 border-[1px] border-slate-300 shadow-md shadow-slate-300 p-2 rounded-lg"
+              src={phone}
+              alt="phone"
+            />
             <div className=" flex flex-col font-bold pl-6">
               <small>Phone</small>
-              <small className=" text-blue-600 tracking-wide">+8801737705511</small>
+              <small className=" text-blue-600 tracking-wide">
+                +8801737705511
+              </small>
             </div>
           </li>
 
           <li className=" flex items-center">
-            <i className="bx h-full bxs-envelope shadow-xl shadow-slate-400 text-2xl rounded-lg  py-2 px-3 bg-gradient-to-r from-[#83c5be] to-[#457b9d]"></i>
+          <img
+              className=" w-12 border-[1px] border-slate-300 shadow-md shadow-slate-300 p-2 rounded-lg"
+              src={email}
+              alt="email"
+            />
             <div className=" flex flex-col justify-between font-bold pl-6">
               <small>Email</small>
-              <small className=" text-blue-600 tracking-wide">mujahidislam400301@gmail.com</small>
+              <small className=" text-blue-600 tracking-wide">
+                mujahidislam400301@gmail.com
+              </small>
             </div>
           </li>
 
           <li className=" flex items-center">
-            <i className="bx bxs-location-plus shadow-xl shadow-slate-400 text-2xl rounded-lg  py-2 px-3 bg-gradient-to-r from-[#83c5be] to-[#457b9d]"></i>
+          <img
+              className=" w-12 border-[1px] border-slate-300 shadow-md shadow-slate-300 p-2 rounded-lg"
+              src={location}
+              alt="location"
+            />
             <div className=" flex flex-col justify-between font-bold pl-6">
               <small>location</small>
-              <small className=" text-blue-600 tracking-wide">Thakurgaon , Rongpur, Bangladesh</small>
+              <small className=" text-blue-600 tracking-wide">
+                Thakurgaon , Rongpur, Bangladesh
+              </small>
             </div>
           </li>
         </ul>
@@ -69,38 +100,29 @@ export default function Sidebar() {
         </button>
       </div>
     </div>
-  )
+  );
 }
 
+// text animation
+import { TypeAnimation } from "react-type-animation";
 
-
-
-
-
-
-
-
-
-// text animation 
-import { TypeAnimation } from 'react-type-animation';
- 
 const ExampleComponent = () => {
   return (
     <TypeAnimation
       sequence={[
-        'frontend web developer', // Types 'One'
+        "frontend web developer", // Types 'One'
         1000, // Waits 1s
-        'Junior Frontend Developer', // Deletes 'One' and types 'Two'
+        "Junior Frontend Developer", // Deletes 'One' and types 'Two'
         1000, // Waits 2s
-        'Frontend Architect', // Types 'Three' without deleting 'Two'
+        "Frontend Architect", // Types 'Three' without deleting 'Two'
         () => {
-          console.log('Sequence completed');
+          console.log("Sequence completed");
         },
       ]}
       wrapper="span"
       cursor={true}
       repeat={Infinity}
-      style={{ fontSize: '18px',fontWeight:'600', display: 'inline-block' }}
+      style={{ fontSize: "18px", fontWeight: "600", display: "inline-block" }}
     />
-  )
-}
+  );
+};
