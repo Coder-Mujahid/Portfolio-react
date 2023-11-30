35c713e4f0f7a5ad67f51f1a197a918225e620c2
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-12-01 00:19:50 +0600

    add new address on map and responsive

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     return (
       <>
       
-          <section className="text-gray-600 body-font relative w-11/12 mx-auto shadow-md shadow-slate-400 rounded-lg">
+          <section className="text-gray-600 body-font md:py-5 relative w-11/12 mx-auto shadow-md shadow-slate-400 rounded-lg">
   
         <div className="container p-4 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden mr-3 p-10 flex items-end justify-start relative">
@@ -12,12 +12,12 @@ const Contact = () => {
               height="100%"
               className="absolute inset-0"
               title="map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d273.33899220314714!2d90.43846892760122!3d23.764721069199826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78c71227c15%3A0x9f0818437919415d!2sAftab%20Nagar%2C%20Dhaka!5e1!3m2!1sen!2sbd!4v1700073366935!5m2!1sen!2sbd"
-            ></iframe>
-            <div className="bg-slate-200 relative flex flex-wrap py-6 rounded shadow-md">
-              <div className="lg:w-1/2 px-6">
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.252142292276!2d88.44041167568027!3d26.038951497092828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e4eb618da025ab%3A0x95cf23c7ca0cb9e3!2sThakurgaon%20Polytechnic%20Institute!5e1!3m2!1sen!2sbd!4v1701368182606!5m2!1sen!2sbd"></iframe>
+            
+            <div className=" bg-slate-100 opacity-90 relative flex flex-wrap py-6 rounded shadow-md">
+              <div className="w-auto px-6">
                 <h2 className="title-font capitalize font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-                <p className="mt-1">Nursery Road , Aftab Nagar ,joharul Haque City , Dhaka</p>
+                <p className="mt-1 text-sm font-semibold text-justify">Gobindo Nagar, Thakurgaon, Rongpur</p>
               </div>
               
             </div>
