7833d5b7ed165d2fee06b3e1c6320a3517092d83
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-12-01 00:10:55 +0600

    responsive  the contact section

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -3,10 +3,10 @@ const Contact = () => {
     return (
       <>
       
-          <section className="text-gray-600 body-font relative w-11/12 mx-auto bg-gradient-to-r from-slate-50 to-slate-300">
+          <section className="text-gray-600 body-font relative w-11/12 mx-auto shadow-md shadow-slate-400 rounded-lg">
   
-        <div className="container p-5 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+        <div className="container p-4 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden mr-3 p-10 flex items-end justify-start relative">
             <iframe
               width="100%"
               height="100%"
@@ -19,21 +19,14 @@ const Contact = () => {
                 <h2 className="title-font capitalize font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
                 <p className="mt-1">Nursery Road , Aftab Nagar ,joharul Haque City , Dhaka</p>
               </div>
-              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-                <a href="#" className="text-indigo-500 leading-relaxed">
-                mujahidislam400301@gmail.com
-                </a>
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-                <p className="leading-relaxed">+8801737705511</p>
-              </div>
+              
             </div>
           </div>
           {/* massage section */}
-          <div className="lg:w-1/3 md:w-1/2 p-4 rounded-md bg-gradient-to-r from-slate-300 to-slate-50 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Contact me </h2>
-            <p className="leading-relaxed mb-5 text-gray-600">If you like to know more about my work or process feel free to get in touch.</p>
-            <div className="relative mb-4">
+          <div className="lg:w-1/3 md:w-1/2 p-4 font-semibold rounded-md bg-gradient-to-r from-slate-300 to-slate-50 flex flex-col md:ml-auto w-full mt-8 md:mt-0">
+            <h2 className="text-gray-900 text-lg font-medium title-font">Contact me </h2>
+            
+            <div className="relative">
               <label htmlFor="name" className="leading-7 text-sm text-gray-600">
                 Name
               </label>
@@ -44,7 +37,7 @@ const Contact = () => {
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <div className="relative mb-4">
+            <div className="relative">
               <label htmlFor="email" className="leading-7 text-sm text-gray-600">
                 Email
               </label>
@@ -66,9 +59,8 @@ const Contact = () => {
               ></textarea>
             </div>
             <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
+              submit
             </button>
-            <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
           </div>
         </div>
       </section>
