01390ab349c59a481f09055c923242df9c36c844
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-12-04 23:17:53 +0600

    change some image

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,4 +1,4 @@
-import home from '../../public/img/icons8-home.gif'
+import home1 from '../../public/img/wired-flat-63-home.gif'
 import skill from '../../public/img/icons8-skill.gif'
 import project from '../../public/img/icons8-project (1).gif'
 import blog from '../../public/img/blogs.gif'
@@ -10,7 +10,7 @@ export default function Menu() {
     <div>
         <ul className=' xl:mt-20 w-full mx-auto md:flex items-center justify-center gap-10 p-3'>
             <Link to={"/"}>
-                <li className=' bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-44 md:h-auto items-center justify-center gap-4 md:gap-2  px-5 py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='w-16' src={home} alt="home" />home</li>
+                <li className=' bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-44 md:h-auto items-center justify-center gap-4 md:gap-2  px-5 py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='w-16' src={home1} alt="home" />home</li>
             </Link>
             
             <Link to={"/Skill"}>
