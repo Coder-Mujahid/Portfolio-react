90032effd4a319e78cf2552fe1c478a764d862ff
mujahidtemp404 <mujahidislam400301@gmail.com>
2024-01-24 00:43:08 +0600

    solve menu button problems

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -8,24 +8,24 @@ import { Link } from 'react-router-dom'
 export default function Menu() {
   return (
     <div>
-        <ul className=' xl:mt-20 w-full mx-auto md:flex items-center justify-center gap-10 p-3'>
+        <ul className=' xl:mt-20 w-full mx-auto grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-cols-3 items-center justify-center gap-3 lg:gap-10 p-3'>
             <Link to={"/"}>
-                <li className=' bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-44 md:h-auto items-center justify-center gap-4 md:gap-2  px-5 py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='w-16' src={home1} alt="home" />home</li>
+                <li className=' md:py-6 p-1 bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-auto items-center justify-center gap-1 md:gap-2  lg:px-5 lg:py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='md:w-16 w-10' src={home1} alt="home" />home</li>
             </Link>
             
             <Link to={"/Skill"}>
-              <li className=' bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-44 md:h-auto items-center justify-center gap-4 md:gap-2  px-5 py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='w-16' src={skill} alt="skill" />skill</li>
+              <li className=' md:py-6 p-1 bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-auto items-center justify-center gap-1 md:gap-2  lg:px-5 lg:py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='md:w-16 w-10' src={skill} alt="skill" />skill</li>
             </Link>
             <Link to={"/Project"}>
-              <li className=' bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-44 md:h-auto items-center justify-center gap-4 md:gap-2  px-5 py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='w-16' src={project} alt="project" />project</li>
+              <li className=' md:py-6 p-1 bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-auto items-center justify-center gap-1 md:gap-2  lg:px-5 lg:py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='md:w-16 w-10' src={project} alt="project" />project</li>
             </Link>
            
             <Link to={"/Blogs"}>
-              <li className=' bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-44 md:h-auto items-center justify-center gap-4 md:gap-2  px-5 py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='w-16' src={blog} alt="blog" />blog</li>
+              <li className=' md:py-6 p-1 bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-auto items-center justify-center gap-1 md:gap-2  lg:px-5 lg:py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='md:w-16 w-10' src={blog} alt="blog" />blog</li>
             </Link>
             
             <Link to={"/Contact"}>
-              <li className=' bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-44 md:h-auto items-center justify-center gap-4 md:gap-2  px-5 py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='w-16' src={contact} alt="contact" />contact</li>
+              <li className=' md:py-6 p-1 bg-white rounded-xl text-center font-semibold capitalize mb-5 md:mb-0 flex flex-col h-auto items-center justify-center gap-1 md:gap-2  lg:px-5 lg:py-3 border-[1px] border-slate-300 shadow-md shadow-slate-300'><img className='md:w-16 w-10' src={contact} alt="contact" />contact</li>
             </Link>
             
         </ul>
